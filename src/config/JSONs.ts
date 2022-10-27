e294cfaea3cb0cd3cdb01437e0c3c28993d11ec6
behzadmor <49172827+behzadmor@users.noreply.github.com>
2022-10-27 21:38:50 +0330

    Add files via upload

--- src/config/JSONs.ts
+++ src/config/JSONs.ts
@@ -1,1 +1,39 @@
+import { Food } from '../data/Food';
 
+const JSONs: Food[] = [
+  {
+    id: 1,
+    name: 'چلو کباب',
+    desc: 'برنج به همراه دو سیخ کباب',
+    price: 100000,
+    picture: '',
+  },
+  {
+    id: 2,
+    name: 'چلو خورشت',
+    desc: 'برنج ایرانی به همراه خورشت سبزی',
+    price: 15000,
+    picture: '',
+  },
+  {
+    id: 3,
+    name: 'پیتزا',
+    desc: 'پیتزا یونانی سایز بزرگ',
+    price: 340000,
+    picture: '',
+  },
+  {
+    id: 4,
+    name: 'ساندویچ',
+    desc: 'ساندویچ سوسیس بزرگ',
+    price: 20000,
+    picture: '',
+  },
+  { id: 5, name: 'کشک بادمجان', desc: '', price: 40000, picture: '' },
+  { id: 6, name: 'ماست', desc: '', price: 4000, picture: '' },
+  { id: 7, name: 'دوغ', desc: '', price: 50000, picture: '' },
+  { id: 8, name: 'نوشابه', desc: '', price: 10000, picture: '' },
+  { id: 9, name: 'آب معدنی', desc: '', price: 7500, picture: '' },
+];
+
+export default JSONs;
